f7446136223345bd6d283cb686dd56c1f2f63bb4
Alan Medina <Alan.Medina@flex.com>
2019-04-13 19:37:32 -0500

    Header Updated

--- src/views/home/HomeView.jsx
+++ src/views/home/HomeView.jsx
@@ -12,7 +12,7 @@
     import logo from '../../img/logo-pokemon.png';
     import { Header } from '../../layouts';
     import PropTypes from 'prop-types';
-    import  'flexboxgrid';
+    import "../../../node_modules/blueprint-css/dist/blueprint.min.css";
 
 // --------------------------------------
 // Create Component Class
@@ -41,6 +41,27 @@
         ** Render Methods
         ** ========================================================================== */
 
+
+            // --------------------------------------
+            // Render Grid Layuot
+            // 1/3 Details View
+            // 2/3 List View
+            // --------------------------------------
+            renderGridLayout() {
+                return (
+                    <div className="row">
+                    <div className = "col-xs-12 col-sm-8 col-md-6 col-lg-4">
+                        <div className="box">12</div>
+                    </div>
+
+                    <div className = "col-xs-12 col-sm-8 col-md-6 col-lg-8">
+                        <div className="box">12</div>
+                    </div>
+                </div>
+                )
+            }
+
+
             // --------------------------------------
             // Render Main View
             // --------------------------------------
@@ -48,6 +69,7 @@
                 return (
                     <Fragment>
                       <Header appLogo = {logo}/>
+                      {this.renderGridLayout()}
                     </Fragment>
                 )
             }
